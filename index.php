27755f2479f22f878c7c212cd65987192e9702f6
odinVos <1197607@student.roc-nijmegen.nl>
2023-03-07 13:25:27 +0100

    opzet paginas
    opzet menu/ login pagina om er later gemakkelijk de database er in te zetten.
    
    home pagina bijna af

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kranenburger</title>
+    <link rel="stylesheet" href="assets/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+        integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@1,300&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Arimo&display=swap" rel="stylesheet">
+</head>
+<body>
+<header>
+        <nav>
+            <ul>
+                <li>
+                    <div class="logo">
+                        <H1> Kranenburger </H1>
+                    </div>  
+                </li>
+                <li id="active"><a href="index.php">Home</a></li>
+                <li><a href="menu.php">Menu</a></li>
+
+                <li class="login"><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="background">
+        <h1 class="name">Cafetaria Kranenburger</h1>
+    </div>
+    <main>
+        <div class="wapen">
+
+        </div>
+        <div class="info">
+            <h1>Over Ons</h1>
+            <br>
+            <hr>
+            <br>
+            <span>Welkom op de website van Kranenburger. <br> Kranenbrger is een cafetaria in Kranenburg(DE). Onze cafetaria bied een wijd asoortiment aan gerechten. </span>
+        </div>
+        <div>
+        </div>
+    </main>
+    <div class="background">
+        <div class="buttons">
+            <a href="menu.php">
+                <button>
+                    <h2>Bekijk menu ></h2>
+                </button>
+            </a>
+        </div>
+    </div>
+    <footer>
+            <span> Cafetaria Kranenburger</span>
+            <div>
+
+                <ul>
+                    <li>
+                        <h3>General info</h3>
+                    </li>
+                    <li><a href="https://github.com/odinVos" target="_blank">Github</a></li>
+                    
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <h3>Contact</h3>
+                    </li>
+                    <li><i class="fas fa-envelope"></i><span> 1197607@student.roc-nijmegen.nl</span></li>
+                    <li><i class="fas fa-phone-alt"></i><span> +31 6 00000</span></li>
+                </ul>
+            </div>
+    </footer>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kranenburger</title>
+    <link rel="stylesheet" href="assets/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+        integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@1,300&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Arimo&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <div class="logo">
+                        <H1> Kranenburger </H1>
+                    </div>  
+                </li>
+                <li id="active"><a href="index.php">Home</a></li>
+                <li><a href="menu.php">Menu</a></li>
+
+                <li class="login"><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="background2">
+        <h1 class="name2">Ons menu</h1>
+
+        <div class="menu-container">
+            <div class="menu">
+
+                <br>
+                <h1>Cafetaria Kranenburger</h1>
+                <h2>Menu</h2>
+                <br>
+                <hr>
+                <span><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></span>
+            </div>
+        </div>
+        <div class="buttons2">
+            <a href="index.php">
+                <button>
+                    <h2>Bestel nu ></h2>
+                </button>
+            </a>
+        </div>
+    </div>
+    <footer>
+            <span> Cafetaria Kranenburger</span>
+            <div>
+
+                <ul>
+                    <li>
+                        <h3>General info</h3>
+                    </li>
+                    <li><a href="https://github.com/odinVos" target="_blank">Github</a></li>
+                    
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <h3>Contact</h3>
+                    </li>
+                    <li><i class="fas fa-envelope"></i><span> 1197607@student.roc-nijmegen.nl</span></li>
+                    <li><i class="fas fa-phone-alt"></i><span> +31 6 00000</span></li>
+                </ul>
+            </div>
+    </footer>
+</body>
+</html>
